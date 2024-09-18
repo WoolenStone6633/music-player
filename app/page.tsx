884516df8e81de28cba404325eaa5fab450338fc
WoolenStone <wph2012.wh@gmail.com>
2024-09-18 11:48:25 -0500

    Refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,13 @@
 'use client'
 
 import LoginButton from "./ui/loginButton";
-import { usePathname, useSearchParams, useRouter } from "next/navigation"
-// import { getTokens } from "@/auth";
-// import { useEffect } from "react";
+import { useSearchParams } from "next/navigation"
 
 export default function Home() {
- const grantType = 'authorization_code'
- const redUri = 'http://localhost:3000/'
+  const grantType = 'authorization_code'
+  const redUri = 'http://localhost:3000/'
 
   const code = useSearchParams().get('code')
-  // const pathname = usePathname()
-  // const { replace } = useRouter()
-  // const parms = new URLSearchParams
-  // parms.set('grant_type', grantType)
-  // code && parms.set('code', code)
-  // parms.set('redirect_uri', redUri)
-  // replace(`${pathname}?${parms.toString()}`)
-
-  // useEffect(() => {
-  //   code && getTokens('')
-  // }, [parms])
 
   return (
     <main className="text-center mt-16">
