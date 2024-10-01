fadfc0ea0eee23e5cd7c6a4e3aa4c7d47d4b7de7
WoolenStone <wph2012.wh@gmail.com>
2024-09-30 23:27:31 -0500

    Tried to fetch data
    Fetching data is correct but not loading correctly on the page

--- app/lib/apiCalls.ts
+++ app/lib/apiCalls.ts
@@ -2,18 +2,35 @@
 
 import { cookies } from "next/headers";
 import { spotifyAuth } from "@/lib/auth";
+const SpotifyWebApi = require('spotify-web-api-node')
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID, 
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET, 
+  redirectUri: process.env.REDIRECT_URI,
+})
 
 export async function getSongs (query: string[] | string | undefined) {
   const refreshToken = cookies().get('RT')?.value
   const accessToken = (await spotifyAuth.refreshAccessToken(refreshToken)).accessToken;
+  spotifyApi.setAccessToken(accessToken)
 
-  const response = await fetch(`https://api.spotify.com/v1/search?q=${query}`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  });
+  var songsArr: any = []
 
-  console.log('This is the result of tracks', response.json())
-  
-  return response.json()
+  spotifyApi.searchTracks(`trask:${query}`)
+    .then((data: any) => {
+      data.body.tracks.items.map((track: any) => {
+        songsArr.push({
+          artist: track.artists.map((artist: any) => {
+            return artist.name
+          }),
+          title: track.name,
+          uri: track.uri,
+          albumUrl: track.album.images[track.album.images.length - 1].url
+        })
+      })
+    }).then( () => {
+      // console.log(songsArr)
+      return songsArr
+    })
 }
