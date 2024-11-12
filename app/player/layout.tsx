b90b9e9027bad72397c568e586723091db645d13
WoolenStone <wph2012.wh@gmail.com>
2024-11-11 20:07:09 -0600

    test for suspense

--- app/player/layout.tsx
+++ app/player/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Search from "../ui/search";
 import SignOutButton from "../ui/signOutButton"
 
@@ -5,7 +6,9 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <>
       <div className="flex justify-end">
-        <Search placeholder="search song or artist"/>
+        <Suspense>
+          <Search placeholder="search song or artist"/>
+        </Suspense>
         <SignOutButton/>
       </div>
       <div className="flex-grow p-12">{children}</div>
