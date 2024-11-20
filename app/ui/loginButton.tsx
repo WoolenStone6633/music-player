5b040d805058a77e7665cb0af2c66507da2dda9a
WoolenStone <wph2012.wh@gmail.com>
2024-11-19 20:43:27 -0600

    Animated loading text
    Animated the dots at the end of the loading text after the user clicks the login button

--- app/ui/loginButton.tsx
+++ app/ui/loginButton.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import gsap from "gsap";
 import { useRef } from "react";
+import SplitType from "split-type";
 
 type props = {
   text: string, 
@@ -11,9 +13,28 @@ export default function LoginButton({text}: props) {
 
   const handleClick = () => {
     if (button.current) {
-      button.current.text = 'Loading...'
+      button.current.text = 'Loading . . .'
       button.current.style.pointerEvents = 'none'
       button.current.style.cursor = 'default'
+      const text = new SplitType(button.current)
+
+      if (text.chars) {
+        text.chars[7].classList.remove('char')
+        text.chars[8].classList.remove('char')
+        text.chars[9].classList.remove('char')
+        text.chars[7].classList.add('period1')
+        text.chars[8].classList.add('period2')
+        text.chars[9].classList.add('period3')
+      }
+
+      const tl = gsap.timeline({repeat: -1, defaults: {duration: 0.2}, repeatDelay: -0.1})
+      const tweenTime = '<0.1'
+      tl.to('.period1', { y: -5}, tweenTime)
+      tl.to('.period2', { y: -5}, tweenTime)
+      tl.to('.period3', { y: -5}, tweenTime)
+      tl.to('.period1', { y: 0}, tweenTime)
+      tl.to('.period2', { y: 0}, tweenTime)
+      tl.to('.period3', { y: 0}, tweenTime)
     }
   }
 
