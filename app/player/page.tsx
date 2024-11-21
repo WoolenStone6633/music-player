9855e78813b3c2f8fc62373de401ba4ac14a5e09
WoolenStone <wph2012.wh@gmail.com>
2024-11-20 21:37:14 -0600

    Added user instructions
    Added a little bit of information for the user so they can understand what to do after logging in

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -26,7 +26,8 @@ export default async function Page({searchParams}: {searchParams?: {query?: stri
         </Suspense>
       </div>
       <div className="w-[592px] opacity-0"/>
-      <div className="fixed mt-28 left-[55%]">
+      <div className="fixed flex flex-col mt-20 left-[55%]">
+        <p className="text-lg mx-auto mb-6 underline underline-offset-8">Start streaming to enjoy the visualizer.</p>
         <StreamProcesser/>
         <SongPlayer accessToken={accessToken} trackUri={searchParams?.id}/>
       </div>
